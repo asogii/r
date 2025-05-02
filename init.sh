e7677f5593ddceb64b5bc2a12db681ae7ed99495
asogii <asogii@163.com>
2025-05-02 11:23:19 +0800

    f

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cd $HOME/opt
 curl -O https://raw.githubusercontent.com/asogii/repo/main/caddy
 curl -O https://raw.githubusercontent.com/asogii/repo/main/nginx
+$HOME/bin/s start
+rm caddy
+rm nginx
 
